f620f517cf6dfe47db2a9e10c467991b36f69f59
rafkastelic <rafaelkastelic@gmail.com>
2022-02-27 15:16:17 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ function mostrarPopup(){
     window.alert("Bem vindos à Games Mania!")
 }
 
+$(document).ready(function(){
+
+    $("#barras").click(function(){
+        if($("#menu").hasClass("#menu-ativo")){
+            $("menu").removeClass("#menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")       
+        }
+    })
+})
